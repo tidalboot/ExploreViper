e51f291d31fcfd4d00840709b77b1b0e68df9cad
Georgios Aikaterinakis <georgios.aikaterinakis@gmail.com>
2019-04-14 10:38:41 +0100

    Add VIPER file structure for module ExploreVenues

---
+++ ExploreViper/ExploreViper/Modules/ExploreVenues/ExploreVenuesInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  ExploreVenuesInteractor.swift
+//  ExploreViper
+//
+//  Created by Georgios Aikaterinakis on 14/04/2019.
+//  Copyright © 2019 Georgios Aikaterinakis. All rights reserved.
+//
+
+import Foundation
---
+++ ExploreViper/ExploreViper/Modules/ExploreVenues/ExploreVenuesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreVenuesViewController.swift
+//  ExploreViper
+//
+//  Created by Georgios Aikaterinakis on 14/04/2019.
+//  Copyright © 2019 Georgios Aikaterinakis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExploreVenuesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
