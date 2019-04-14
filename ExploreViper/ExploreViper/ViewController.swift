aefb320ccb34637c965e1becdca77faa146c4195
Georgios Aikaterinakis <georgios.aikaterinakis@gmail.com>
2019-04-14 10:20:24 +0100

    Create ExploreViper project

---
+++ ExploreViper/ExploreViper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExploreViper
+//
+//  Created by Georgios Aikaterinakis on 14/04/2019.
+//  Copyright © 2019 Georgios Aikaterinakis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
